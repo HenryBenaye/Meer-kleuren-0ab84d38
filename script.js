d10005a948489fb0e64f8207f3252cb275c42ec4
HenryBenaye <henry.be@outlook.com>
2020-05-25 12:55:02 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+var backgroundColor;
+var titleColor;
+function saveColorAsBackround() {
+    backgroundColor = document.getElementById("colorChoice").value;
+    console.log(backgroundColor);
+
+
+}
+
 function knop() {
-    var color = document.getElementById("colorChoice").value;
-    document.body.style.backgroundColor = color;
-    document.querySelector("body").innerText= "Verander achtergrond kleur!";
+    document.getElementById("txt").style.color= titleColor; 
+    document.body.style.backgroundColor = backgroundColor;
+}
+
+function saveT() {
+    titleColor = document.getElementById("colorChoice").value;
+    console.log(titleColor);
 }
